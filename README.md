429d1f97af2ff378f2a0bbf18b09766a66a3fff7
Vasilios Magriplis <etameson@gmail.com>
2021-05-11 16:00:01 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # Basic Windows Drivers
 
+## Intro
+
 Basic experimentation with Windows drivers [based off the MSDN docs](https://docs.microsoft.com/en-us/windows-hardware/drivers/gettingstarted/writing-a-very-small-kmdf--driver).
+
+## Debugging
+
+You'll need to first configure a target machine or VM for deployment and debugging. [See here](https://docs.microsoft.com/en-us/windows-hardware/drivers/gettingstarted/provision-a-target-computer-wdk-8-1). Note that you need to ensure the target and host can ping each other. You may need to change the VM to use a Bridged connection and allow [File and Printer Sharing](https://stackoverflow.com/questions/18278409/cant-ping-a-local-vm-from-the-host) on both the host and the target. You may then need to set up your [Debug Print Filter](https://stackoverflow.com/questions/17109074/kdprintex-in-debugger-immediate-window-into-vs-2012-is-not-printing-any-msg). Once deployed and installed on the target, you'll need to start WndDbg session e.g. `WinDbg -k net:port=<port>,key=<key>`. WinDbg is located in your Windows kit, e.g. `A:\Windows Kits\10\Debuggers\x64`. Note that `KdPrintEx` does not appear to do anything in `DriverEntry()`, so don't expect output when walking through this symbol.
+
+## Other notes
+
+* If devcon.exe fails, a log is generated at `c:\windows\inf\setupapi.dev.log`.
+
+## Resource
+
+* [Download the WDK](https://docs.microsoft.com/en-us/windows-hardware/drivers/download-the-wdk)
+* [WinDbg basics](https://www.tenforums.com/tutorials/5558-windbg-basics-debugging-crash-dumps-windows-10-a.html)
+* [Debug print message issues](https://social.msdn.microsoft.com/Forums/vstudio/en-US/4ec8c0fd-c399-4579-ac0b-d5d263820511/where-can-i-see-the-kdprintex-debug-message-in-vs-debugger-for-the-kmdfhelloworld-sample-project).
+* [Reading and Filtering Debugging Messages](https://docs.microsoft.com/en-us/windows-hardware/drivers/debugger/reading-and-filtering-debugging-messages) (see also [this](https://stackoverflow.com/questions/17109074/kdprintex-in-debugger-immediate-window-into-vs-2012-is-not-printing-any-msg)).
+* [Debug Windows Drivers - Step by Step Lab (Echo Kernel-Mode)](https://docs.microsoft.com/en-us/windows-hardware/drivers/debugger/debug-universal-drivers---step-by-step-lab--echo-kernel-mode-).
+* [Getting Started with Windows Drivers](https://docs.microsoft.com/en-us/windows-hardware/drivers/develop/getting-started-with-windows-drivers).
+* [Sample KMDF drivers](https://docs.microsoft.com/en-us/windows-hardware/drivers/wdf/sample-kmdf-drivers)
+* [Windows Driver Samples](https://github.com/microsoft/Windows-driver-samples)
+* [Debug View](https://docs.microsoft.com/en-us/sysinternals/downloads/debugview)
+* [Getting Started with WinDbg (User-Mode)](https://docs.microsoft.com/en-us/windows-hardware/drivers/debugger/getting-started-with-windbg)
---
+++ src/KmdfHelloWorld/KmdfHelloWorld/Driver.c
@@ -0,0 +1,98 @@
+#include <ntddk.h>
+#include <wdf.h>
+
+// Based on https://docs.microsoft.com/en-us/windows-hardware/drivers/gettingstarted/writing-a-very-small-kmdf--driver
+
+DRIVER_INITIALIZE DriverEntry;
+EVT_WDF_DRIVER_DEVICE_ADD KmdfHelloWorldEvtDeviceAdd;
+
+/*
+* DriverEntry is the entry point for all drivers, like Main() is for many user mode applications. 
+* The job of DriverEntry is to initialize driver-wide structures and resources. In this example, 
+* you printed "Hello World" for DriverEntry, configured the driver object to register your 
+* EvtDeviceAdd callback's entry point, then created the driver object and returned.
+* 
+* The driver object acts as the parent object for all other framework objects you might create in 
+* your driver, which include device objects, I/O queues, timers, spinlocks, and more. For more 
+* information about framework objects, see Introduction to Framework Objects.
+* 
+* For DriverEntry, we strongly recommend keeping the name as "DriverEntry" to help with code 
+* analysis and debugging.
+*/
+NTSTATUS
+DriverEntry(
+    _In_ PDRIVER_OBJECT     DriverObject,
+    _In_ PUNICODE_STRING    RegistryPath
+)
+{
+    // NTSTATUS variable to record success or failure
+    NTSTATUS status = STATUS_SUCCESS;
+
+    // Allocate the driver configuration object
+    WDF_DRIVER_CONFIG config;
+
+    // Print "Hello World" for DriverEntry
+    KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "KmdfHelloWorld: DriverEntry\n"));
+
+    // Initialize the driver configuration object to register the
+    // entry point for the EvtDeviceAdd callback, KmdfHelloWorldEvtDeviceAdd
+    WDF_DRIVER_CONFIG_INIT(&config,
+        KmdfHelloWorldEvtDeviceAdd
+    );
+
+    // Finally, create the driver object
+    status = WdfDriverCreate(DriverObject,
+        RegistryPath,
+        WDF_NO_OBJECT_ATTRIBUTES,
+        &config,
+        WDF_NO_HANDLE
+    );
+    return status;
+}
+
+/*
+* EvtDeviceAdd is invoked by the system when it detects that your device has arrived. Its job 
+* is to initialize structuresand resources for that device.In this example, you simply printed 
+* out a "Hello World" message for EvtDeviceAdd, created the device object, and returned. In 
+* other drivers you write, you might create I / O queues for your hardware, set up a device 
+* context storage space for device - specific information, or perform other tasks needed to 
+* prepare your device.
+* 
+* For the device add callback, notice how you named it with your driver's name as a prefix 
+* (KmdfHelloWorldEvtDeviceAdd). Generally, we recommend naming your driver's functions in this 
+* way to differentiate them from other drivers' functions. DriverEntry is the only one you 
+* should name exactly that.
+*/
+NTSTATUS
+KmdfHelloWorldEvtDeviceAdd(
+    _In_    WDFDRIVER       Driver,
+    _Inout_ PWDFDEVICE_INIT DeviceInit
+)
+{
+    // We're not using the driver object,
+    // so we need to mark it as unreferenced
+    UNREFERENCED_PARAMETER(Driver);
+
+    NTSTATUS status;
+
+    // Allocate the device object
+    WDFDEVICE hDevice;
+
+    // Print "Hello World"
+    KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "KmdfHelloWorld: KmdfHelloWorldEvtDeviceAdd\n"));
+
+    // Create the device object
+    status = WdfDeviceCreate(&DeviceInit,
+        WDF_NO_OBJECT_ATTRIBUTES,
+        &hDevice
+    );
+    return status;
+}
+
+/*
+* This example illustrates a fundamental concept of drivers: they are a "collection of callbacks" that, 
+* once initialized, sit and wait for the system to call them when it needs something. This could be a 
+* new device arrival event, an I/O request from a user mode application, a system power shutdown event, 
+* a request from another driver, or a surprise removal event when a user unplugs the device unexpectedly. 
+* Fortunately, to say "Hello World," you only needed to worry about driver and device creation.
+*/
