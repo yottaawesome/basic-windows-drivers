d8e93789a05c118bd4583c1f66afd2945aa44b3e
Vasilios Magriplis <etameson@gmail.com>
2021-05-30 14:25:24 +1000

    Continue filter

--- src/FilterDriver/NfdCore.hpp
+++ src/FilterDriver/NfdCore.hpp
@@ -3,6 +3,41 @@
 //#include <ntddk.h>
 #include <ndis.h>
 
+typedef NDIS_SPIN_LOCK      FILTER_LOCK;
+typedef PNDIS_SPIN_LOCK     PFILTER_LOCK;
+
+#define FILTER_LOG_RCV_REF(_O, _Instance, _NetBufferList, _Ref)
+
+#define FILTER_INIT_LOCK(_pLock)      NdisAllocateSpinLock(_pLock)
+
+#define FILTER_FREE_LOCK(_pLock)      NdisFreeSpinLock(_pLock)
+
+#define FILTER_LOG_SEND_REF(_O, _Instance, _NetBufferList, _Ref)
+
+#define FILTER_ACQUIRE_LOCK(_pLock, DispatchLevel)              \
+    {                                                           \
+        if (DispatchLevel)                                      \
+        {                                                       \
+            NdisDprAcquireSpinLock(_pLock);                     \
+        }                                                       \
+        else                                                    \
+        {                                                       \
+            NdisAcquireSpinLock(_pLock);                        \
+        }                                                       \
+    }
+
+#define FILTER_RELEASE_LOCK(_pLock, DispatchLevel)              \
+    {                                                           \
+        if (DispatchLevel)                                      \
+        {                                                       \
+            NdisDprReleaseSpinLock(_pLock);                     \
+        }                                                       \
+        else                                                    \
+        {                                                       \
+            NdisReleaseSpinLock(_pLock);                        \
+        }                                                       \
+    }
+
 const wchar_t* FILTER_FRIENDLY_NAME = L"NDIS Sample LightWeight Filter";
 // TODO: Customize this to match the service name in the INF
 const wchar_t* FILTER_SERVICE_NAME = L"NDISLWF";
@@ -19,3 +54,116 @@ typedef struct _FILTER_DEVICE_EXTENSION
     ULONG            Signature;
     NDIS_HANDLE      Handle;
 } FILTER_DEVICE_EXTENSION, * PFILTER_DEVICE_EXTENSION;
+
+typedef struct _QUEUE_ENTRY
+{
+    struct _QUEUE_ENTRY* Next;
+}QUEUE_ENTRY, * PQUEUE_ENTRY;
+
+typedef enum _FILTER_STATE
+{
+    FilterStateUnspecified,
+    FilterInitialized,
+    FilterPausing,
+    FilterPaused,
+    FilterRunning,
+    FilterRestarting,
+    FilterDetaching
+} FILTER_STATE;
+
+typedef struct _QUEUE_HEADER
+{
+    PQUEUE_ENTRY     Head;
+    PQUEUE_ENTRY     Tail;
+} QUEUE_HEADER, PQUEUE_HEADER;
+
+typedef struct _MS_FILTER
+{
+    LIST_ENTRY                     FilterModuleLink;
+    //Reference to this filter
+    ULONG                           RefCount;
+
+    NDIS_HANDLE                     FilterHandle;
+    NDIS_STRING                     FilterModuleName;
+    NDIS_STRING                     MiniportFriendlyName;
+    NDIS_STRING                     MiniportName;
+    NET_IFINDEX                     MiniportIfIndex;
+
+    NDIS_STATUS                     Status;
+    NDIS_EVENT                      Event;
+    ULONG                           BackFillSize;
+    NDIS_SPIN_LOCK                     Lock;    // Lock for protection of state and outstanding sends and recvs
+
+    FILTER_STATE                    State;   // Which state the filter is in
+    ULONG                           OutstandingSends;
+    ULONG                           OutstandingRequest;
+    ULONG                           OutstandingRcvs;
+    NDIS_SPIN_LOCK                     SendLock;
+    NDIS_SPIN_LOCK                     RcvLock;
+    QUEUE_HEADER                    SendNBLQueue;
+    QUEUE_HEADER                    RcvNBLQueue;
+
+
+    NDIS_STRING                     FilterName;
+    ULONG                           CallsRestart;
+    BOOLEAN                         TrackReceives;
+    BOOLEAN                         TrackSends;
+#if DBG
+    BOOLEAN                         bIndicating;
+#endif
+
+    PNDIS_OID_REQUEST               PendingOidRequest;
+
+}MS_FILTER, * PMS_FILTER;
+
+NTSTATUS
+FilterDispatch(
+    PDEVICE_OBJECT       DeviceObject,
+    PIRP                 Irp
+);
+
+NDIS_STATUS
+FilterRegisterOptions(
+    NDIS_HANDLE  NdisFilterDriverHandle,
+    NDIS_HANDLE  FilterDriverContext
+);
+
+NTSTATUS
+FilterDeviceIoControl(
+    PDEVICE_OBJECT        DeviceObject,
+    PIRP                  Irp
+);
+
+_IRQL_requires_max_(PASSIVE_LEVEL)
+NDIS_STATUS
+FilterRegisterDevice(
+    VOID
+);
+
+NDIS_STATUS
+FilterSetModuleOptions(
+    NDIS_HANDLE             FilterModuleContext
+);
+
+VOID FilterSendNetBufferListsComplete(
+    NDIS_HANDLE         FilterModuleContext,
+    PNET_BUFFER_LIST    NetBufferLists,
+    ULONG               SendCompleteFlags
+);
+
+VOID
+FilterSendNetBufferLists(
+    NDIS_HANDLE         FilterModuleContext,
+    PNET_BUFFER_LIST    NetBufferLists,
+    NDIS_PORT_NUMBER    PortNumber,
+    ULONG               SendFlags
+);
+
+VOID
+FilterReceiveNetBufferLists(
+    NDIS_HANDLE         FilterModuleContext,
+    PNET_BUFFER_LIST    NetBufferLists,
+    NDIS_PORT_NUMBER    PortNumber,
+    ULONG               NumberOfNetBufferLists,
+    ULONG               ReceiveFlags
+);
--- src/FilterDriver/main.cpp
+++ src/FilterDriver/main.cpp
@@ -2,10 +2,312 @@
 
 NDIS_HANDLE         FilterDriverHandle; // NDIS handle for filter driver
 NDIS_HANDLE         FilterDriverObject;
-NDIS_SPIN_LOCK         FilterListLock;
+FILTER_LOCK         FilterListLock;
 LIST_ENTRY          FilterModuleList;
 NDIS_HANDLE         NdisFilterDeviceHandle = NULL;
 PDEVICE_OBJECT      NdisDeviceObject = NULL;
+
+_Use_decl_annotations_
+VOID FilterSendNetBufferListsComplete(
+	NDIS_HANDLE         FilterModuleContext,
+	PNET_BUFFER_LIST    NetBufferLists,
+	ULONG               SendCompleteFlags
+)
+{
+	PMS_FILTER         pFilter = (PMS_FILTER)FilterModuleContext;
+	ULONG              NumOfSendCompletes = 0;
+	BOOLEAN            DispatchLevel;
+	PNET_BUFFER_LIST   CurrNbl;
+
+	//
+	// If your filter injected any send packets into the datapath to be sent,
+	// you must identify their NBLs here and remove them from the chain.  Do not
+	// attempt to send-complete your NBLs up to the higher layer.
+	//
+
+	//
+	// If your filter has modified any NBLs (or NBs, MDLs, etc) in your
+	// FilterSendNetBufferLists handler, you must undo the modifications here.
+	// In general, NBLs must be returned in the same condition in which you had
+	// you received them.  (Exceptions: the NBLs can be re-ordered on the linked
+	// list, and the scratch fields are don't-care).
+	//
+
+	if (pFilter->TrackSends)
+	{
+		CurrNbl = NetBufferLists;
+		while (CurrNbl)
+		{
+			NumOfSendCompletes++;
+			CurrNbl = NET_BUFFER_LIST_NEXT_NBL(CurrNbl);
+
+		}
+		DispatchLevel = NDIS_TEST_SEND_AT_DISPATCH_LEVEL(SendCompleteFlags);
+		FILTER_ACQUIRE_LOCK(&pFilter->Lock, DispatchLevel);
+		pFilter->OutstandingSends -= NumOfSendCompletes;
+		FILTER_LOG_SEND_REF(2, pFilter, PrevNbl, pFilter->OutstandingSends);
+		FILTER_RELEASE_LOCK(&pFilter->Lock, DispatchLevel);
+	}
+
+	// Send complete the NBLs.  If you removed any NBLs from the chain, make
+	// sure the chain isn't empty (i.e., NetBufferLists!=NULL).
+
+	NdisFSendNetBufferListsComplete(pFilter->FilterHandle, NetBufferLists, SendCompleteFlags);
+}
+
+_Use_decl_annotations_
+VOID
+FilterSendNetBufferLists(
+	NDIS_HANDLE         FilterModuleContext,
+	PNET_BUFFER_LIST    NetBufferLists,
+	NDIS_PORT_NUMBER    PortNumber,
+	ULONG               SendFlags
+)
+{
+	PMS_FILTER          pFilter = (PMS_FILTER)FilterModuleContext;
+	PNET_BUFFER_LIST    CurrNbl;
+	BOOLEAN             DispatchLevel;
+	BOOLEAN             bFalse = FALSE;
+
+	do
+	{
+
+		DispatchLevel = NDIS_TEST_SEND_AT_DISPATCH_LEVEL(SendFlags);
+#if DBG
+		//
+		// we should never get packets to send if we are not in running state
+		//
+
+		FILTER_ACQUIRE_LOCK(&pFilter->Lock, DispatchLevel);
+		//
+		// If the filter is not in running state, fail the send
+		//
+		if (pFilter->State != FilterRunning)
+		{
+			FILTER_RELEASE_LOCK(&pFilter->Lock, DispatchLevel);
+
+			CurrNbl = NetBufferLists;
+			while (CurrNbl)
+			{
+				NET_BUFFER_LIST_STATUS(CurrNbl) = NDIS_STATUS_PAUSED;
+				CurrNbl = NET_BUFFER_LIST_NEXT_NBL(CurrNbl);
+			}
+			NdisFSendNetBufferListsComplete(pFilter->FilterHandle,
+				NetBufferLists,
+				DispatchLevel ? NDIS_SEND_COMPLETE_FLAGS_DISPATCH_LEVEL : 0);
+			break;
+
+		}
+		FILTER_RELEASE_LOCK(&pFilter->Lock, DispatchLevel);
+#endif
+		if (pFilter->TrackSends)
+		{
+			FILTER_ACQUIRE_LOCK(&pFilter->Lock, DispatchLevel);
+			CurrNbl = NetBufferLists;
+			while (CurrNbl)
+			{
+				pFilter->OutstandingSends++;
+				FILTER_LOG_SEND_REF(1, pFilter, CurrNbl, pFilter->OutstandingSends);
+
+				CurrNbl = NET_BUFFER_LIST_NEXT_NBL(CurrNbl);
+			}
+			FILTER_RELEASE_LOCK(&pFilter->Lock, DispatchLevel);
+		}
+
+		//
+		// If necessary, queue the NetBufferLists in a local structure for later
+		// processing.  However, do not queue them for "too long", or else the
+		// system's performance may be degraded.  If you need to hold onto an
+		// NBL for an unbounded amount of time, then allocate memory, perform a
+		// deep copy, and complete the original NBL.
+		//
+
+		NdisFSendNetBufferLists(pFilter->FilterHandle, NetBufferLists, PortNumber, SendFlags);
+
+
+	} while (bFalse);
+}
+
+_Use_decl_annotations_
+VOID
+FilterReceiveNetBufferLists(
+	NDIS_HANDLE         FilterModuleContext,
+	PNET_BUFFER_LIST    NetBufferLists,
+	NDIS_PORT_NUMBER    PortNumber,
+	ULONG               NumberOfNetBufferLists,
+	ULONG               ReceiveFlags
+)
+{
+
+	PMS_FILTER          pFilter = (PMS_FILTER)FilterModuleContext;
+	BOOLEAN             DispatchLevel;
+	ULONG               Ref;
+	BOOLEAN             bFalse = FALSE;
+#if DBG
+	ULONG               ReturnFlags;
+#endif
+	do
+	{
+
+		DispatchLevel = NDIS_TEST_RECEIVE_AT_DISPATCH_LEVEL(ReceiveFlags);
+#if DBG
+		FILTER_ACQUIRE_LOCK(&pFilter->Lock, DispatchLevel);
+
+		if (pFilter->State != FilterRunning)
+		{
+			FILTER_RELEASE_LOCK(&pFilter->Lock, DispatchLevel);
+
+			if (NDIS_TEST_RECEIVE_CAN_PEND(ReceiveFlags))
+			{
+				ReturnFlags = 0;
+				if (NDIS_TEST_RECEIVE_AT_DISPATCH_LEVEL(ReceiveFlags))
+				{
+					NDIS_SET_RETURN_FLAG(ReturnFlags, NDIS_RETURN_FLAGS_DISPATCH_LEVEL);
+				}
+
+				NdisFReturnNetBufferLists(pFilter->FilterHandle, NetBufferLists, ReturnFlags);
+			}
+			break;
+		}
+		FILTER_RELEASE_LOCK(&pFilter->Lock, DispatchLevel);
+#endif
+
+		ASSERT(NumberOfNetBufferLists >= 1);
+
+		//
+		// If you would like to drop a received packet, then you must carefully
+		// modify the NBL chain as follows:
+		//
+		//     if NDIS_TEST_RECEIVE_CANNOT_PEND(ReceiveFlags):
+		//         For each NBL that is NOT dropped, temporarily unlink it from
+		//         the linked list, and indicate it up alone with 
+		//         NdisFIndicateReceiveNetBufferLists and the
+		//         NDIS_RECEIVE_FLAGS_RESOURCES flag set.  Then immediately
+		//         relink the NBL back into the chain.  When all NBLs have been
+		//         indicated up, you may return from this function.
+		//     otherwise (NDIS_TEST_RECEIVE_CANNOT_PEND is FALSE):
+		//         Divide the linked list of NBLs into two chains: one chain
+		//         of packets to drop, and everything else in another chain.
+		//         Return the first chain with NdisFReturnNetBufferLists, and
+		//         indicate up the rest with NdisFIndicateReceiveNetBufferLists.
+		//
+		// Note: on the receive path for Ethernet packets, one NBL will have 
+		// exactly one NB.  So (assuming you are receiving on Ethernet, or are 
+		// attached above Native WiFi) you do not need to worry about dropping
+		// one NB, but trying to indicate up the remaining NBs on the same NBL.
+		// In other words, if the first NB should be dropped, drop the whole NBL.
+		//
+
+		//
+		// If you would like to modify a packet, and can do so quickly, you may
+		// do it here.  However, make sure you save enough information to undo
+		// your modification in the FilterReturnNetBufferLists handler.
+		//
+
+		//
+		// If necessary, queue the NetBufferLists in a local structure for later
+		// processing.  However, do not queue them for "too long", or else the
+		// system's performance may be degraded.  If you need to hold onto an
+		// NBL for an unbounded amount of time, then allocate memory, perform a
+		// deep copy, and return the original NBL.
+		//
+
+		if (pFilter->TrackReceives)
+		{
+			FILTER_ACQUIRE_LOCK(&pFilter->Lock, DispatchLevel);
+			pFilter->OutstandingRcvs += NumberOfNetBufferLists;
+			Ref = pFilter->OutstandingRcvs;
+
+			FILTER_LOG_RCV_REF(1, pFilter, NetBufferLists, Ref);
+			FILTER_RELEASE_LOCK(&pFilter->Lock, DispatchLevel);
+		}
+
+		NdisFIndicateReceiveNetBufferLists(
+			pFilter->FilterHandle,
+			NetBufferLists,
+			PortNumber,
+			NumberOfNetBufferLists,
+			ReceiveFlags);
+
+
+		if (NDIS_TEST_RECEIVE_CANNOT_PEND(ReceiveFlags) &&
+			pFilter->TrackReceives)
+		{
+			FILTER_ACQUIRE_LOCK(&pFilter->Lock, DispatchLevel);
+			pFilter->OutstandingRcvs -= NumberOfNetBufferLists;
+			Ref = pFilter->OutstandingRcvs;
+			FILTER_LOG_RCV_REF(2, pFilter, NetBufferLists, Ref);
+			FILTER_RELEASE_LOCK(&pFilter->Lock, DispatchLevel);
+		}
+
+	} while (bFalse);
+}
+
+_Use_decl_annotations_
+VOID
+FilterReturnNetBufferLists(
+	NDIS_HANDLE         FilterModuleContext,
+	PNET_BUFFER_LIST    NetBufferLists,
+	ULONG               ReturnFlags
+)
+{
+	PMS_FILTER          pFilter = (PMS_FILTER)FilterModuleContext;
+	PNET_BUFFER_LIST    CurrNbl = NetBufferLists;
+	UINT                NumOfNetBufferLists = 0;
+	BOOLEAN             DispatchLevel;
+	ULONG               Ref;
+
+	//
+	// If your filter injected any receive packets into the datapath to be
+	// received, you must identify their NBLs here and remove them from the 
+	// chain.  Do not attempt to receive-return your NBLs down to the lower
+	// layer.
+	//
+
+	//
+	// If your filter has modified any NBLs (or NBs, MDLs, etc) in your
+	// FilterReceiveNetBufferLists handler, you must undo the modifications here.
+	// In general, NBLs must be returned in the same condition in which you had
+	// you received them.  (Exceptions: the NBLs can be re-ordered on the linked
+	// list, and the scratch fields are don't-care).
+	//
+
+	if (pFilter->TrackReceives)
+	{
+		while (CurrNbl)
+		{
+			NumOfNetBufferLists++;
+			CurrNbl = NET_BUFFER_LIST_NEXT_NBL(CurrNbl);
+		}
+	}
+
+
+	// Return the received NBLs.  If you removed any NBLs from the chain, make
+	// sure the chain isn't empty (i.e., NetBufferLists!=NULL).
+
+	NdisFReturnNetBufferLists(pFilter->FilterHandle, NetBufferLists, ReturnFlags);
+
+	if (pFilter->TrackReceives)
+	{
+		DispatchLevel = NDIS_TEST_RETURN_AT_DISPATCH_LEVEL(ReturnFlags);
+		FILTER_ACQUIRE_LOCK(&pFilter->Lock, DispatchLevel);
+
+		pFilter->OutstandingRcvs -= NumOfNetBufferLists;
+		Ref = pFilter->OutstandingRcvs;
+		FILTER_LOG_RCV_REF(3, pFilter, NetBufferLists, Ref);
+		FILTER_RELEASE_LOCK(&pFilter->Lock, DispatchLevel);
+	}
+}
+
+NDIS_FILTER_PARTIAL_CHARACTERISTICS DefaultChars = {
+{ 0, 0, 0},
+	  0,
+	  FilterSendNetBufferLists,
+	  FilterSendNetBufferListsComplete,
+	  NULL,
+	  FilterReceiveNetBufferLists,
+	  FilterReturnNetBufferLists
+};
 
 extern "C" NTSTATUS DriverEntry(
 	_In_ PDRIVER_OBJECT driverObject,
@@ -49,6 +351,26 @@ FilterDispatch(
 	IoCompleteRequest(Irp, IO_NO_INCREMENT);
 
 	return Status;
+}
+
+_Use_decl_annotations_
+NDIS_STATUS
+FilterRegisterOptions(
+	NDIS_HANDLE  NdisFilterDriverHandle,
+	NDIS_HANDLE  FilterDriverContext
+)
+{
+
+	ASSERT(NdisFilterDriverHandle == FilterDriverHandle);
+	ASSERT(FilterDriverContext == (NDIS_HANDLE)FilterDriverObject);
+
+	if ((NdisFilterDriverHandle != (NDIS_HANDLE)FilterDriverHandle) ||
+		(FilterDriverContext != (NDIS_HANDLE)FilterDriverObject))
+	{
+		return NDIS_STATUS_INVALID_PARAMETER;
+	}
+
+	return NDIS_STATUS_SUCCESS;
 }
 
 _Use_decl_annotations_
@@ -126,6 +448,85 @@ FilterRegisterDevice(
 
 }
 
+_Use_decl_annotations_
+NDIS_STATUS
+FilterSetModuleOptions(
+	NDIS_HANDLE             FilterModuleContext
+)
+{
+	PMS_FILTER                               pFilter = (PMS_FILTER)FilterModuleContext;
+	NDIS_FILTER_PARTIAL_CHARACTERISTICS      OptionalHandlers;
+	NDIS_STATUS                              Status = NDIS_STATUS_SUCCESS;
+	BOOLEAN                                  bFalse = FALSE;
+
+	//
+	// Demonstrate how to change send/receive handlers at runtime.
+	//
+	if (bFalse)
+	{
+		UINT      i;
+
+
+		pFilter->CallsRestart++;
+
+		i = pFilter->CallsRestart % 8;
+
+		pFilter->TrackReceives = TRUE;
+		pFilter->TrackSends = TRUE;
+
+		NdisMoveMemory(&OptionalHandlers, &DefaultChars, sizeof(OptionalHandlers));
+		OptionalHandlers.Header.Type = NDIS_OBJECT_TYPE_FILTER_PARTIAL_CHARACTERISTICS;
+		OptionalHandlers.Header.Size = sizeof(OptionalHandlers);
+		switch (i)
+		{
+
+		case 0:
+			OptionalHandlers.ReceiveNetBufferListsHandler = NULL;
+			pFilter->TrackReceives = FALSE;
+			break;
+
+		case 1:
+
+			OptionalHandlers.ReturnNetBufferListsHandler = NULL;
+			pFilter->TrackReceives = FALSE;
+			break;
+
+		case 2:
+			OptionalHandlers.SendNetBufferListsHandler = NULL;
+			pFilter->TrackSends = FALSE;
+			break;
+
+		case 3:
+			OptionalHandlers.SendNetBufferListsCompleteHandler = NULL;
+			pFilter->TrackSends = FALSE;
+			break;
+
+		case 4:
+			OptionalHandlers.ReceiveNetBufferListsHandler = NULL;
+			OptionalHandlers.ReturnNetBufferListsHandler = NULL;
+			break;
+
+		case 5:
+			OptionalHandlers.SendNetBufferListsHandler = NULL;
+			OptionalHandlers.SendNetBufferListsCompleteHandler = NULL;
+			break;
+
+		case 6:
+
+			OptionalHandlers.ReceiveNetBufferListsHandler = NULL;
+			OptionalHandlers.ReturnNetBufferListsHandler = NULL;
+			OptionalHandlers.SendNetBufferListsHandler = NULL;
+			OptionalHandlers.SendNetBufferListsCompleteHandler = NULL;
+			break;
+
+		case 7:
+			break;
+		}
+		Status = NdisSetOptionalHandlers(pFilter->FilterHandle, (PNDIS_DRIVER_OPTIONAL_HANDLERS)&OptionalHandlers);
+	}
+	return Status;
+}
+
 NTSTATUS DriverEntry(
 	_In_ PDRIVER_OBJECT driverObject,
 	_In_ PUNICODE_STRING registryPath
@@ -160,12 +561,14 @@ NTSTATUS DriverEntry(
 	// handler, set it to NULL and NDIS will more efficiently pass the
 	// operation through on your behalf.
 	//
-	/*FChars.SetOptionsHandler = FilterRegisterOptions;
-	FChars.AttachHandler = FilterAttach;
-	FChars.DetachHandler = FilterDetach;
-	FChars.RestartHandler = FilterRestart;
-	FChars.PauseHandler = FilterPause;
-	FChars.SetFilterModuleOptionsHandler = FilterSetModuleOptions;
+	//FChars.SetOptionsHandler = FilterRegisterOptions;
+	//FChars.SetFilterModuleOptionsHandler = FilterSetModuleOptions;
+	//FChars.AttachHandler = FilterAttach;
+	//FChars.DetachHandler = FilterDetach;
+	//FChars.RestartHandler = FilterRestart;
+	//FChars.PauseHandler = FilterPause;
+
+	/*
 	FChars.OidRequestHandler = FilterOidRequest;
 	FChars.OidRequestCompleteHandler = FilterOidRequestComplete;
 	FChars.CancelOidRequestHandler = FilterCancelOidRequest;
@@ -186,7 +589,7 @@ NTSTATUS DriverEntry(
 	//
 	// Initialize spin locks
 	//
-	NdisAllocateSpinLock(&FilterListLock);
+	FILTER_INIT_LOCK(&FilterListLock);
 
 	InitializeListHead(&FilterModuleList);
 
@@ -205,7 +608,7 @@ NTSTATUS DriverEntry(
 	if (Status != NDIS_STATUS_SUCCESS)
 	{
 		NdisFDeregisterFilterDriver(FilterDriverHandle);
-		NdisFreeSpinLock(&FilterListLock);
+		FILTER_FREE_LOCK(&FilterListLock);
 		//DEBUGP(DL_WARN, "Register device for the filter driver failed.\n");
 	}
 
