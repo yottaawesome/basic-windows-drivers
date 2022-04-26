f176b4b554f6ab13494ad65f5e5b55d9ab46ef1b
Vasilios Magriplis <etameson@gmail.com>
2022-04-26 20:17:50 +1000

    Minor changes

--- src/SampleWdmDriver/include/ScopedSpinLock.hpp
+++ src/SampleWdmDriver/include/ScopedSpinLock.hpp
@@ -1,6 +1,12 @@
 #pragma once
 #include <Ntddk.h>
 
+/* 
+* Note the following limitations from MSDN: https://docs.microsoft.com/en-us/windows-hardware/drivers/ddi/wdm/nf-wdm-keacquirespinlock
+	* The code within a critical region guarded by an spin lock must neither be pageable nor make any references to pageable data.
+	* The code within a critical region guarded by a spin lock can neither call any external function that might access pageable data or raise an exception, nor can it generate any exceptions.
+	* The caller should release the spin lock with KeReleaseSpinLock as quickly as possible.
+*/
 class ScopedSpinLock
 {
 	public:
