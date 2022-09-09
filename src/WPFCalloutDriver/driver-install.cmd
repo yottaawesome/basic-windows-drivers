55c349bcaa0e69e56558ab55e15287de01775edb
Vasilios Magriplis <etameson@gmail.com>
2022-09-09 18:10:38 +1000

    Minor improvements

--- src/WPFCalloutDriver/driver-install.cmd
+++ src/WPFCalloutDriver/driver-install.cmd
@@ -1,3 +1,5 @@
+@echo off
+
 SET SIGNTOOL_PATH="C:\Program Files (x86)\Windows Kits\10\App Certification Kit"
 SET DRIVER_BIN=WFPCalloutDriver.sys
 SET DRIVER_INF=WFPCalloutDriver.inf
@@ -9,10 +11,16 @@ IF /I "%1" EQU "r"  ( GOTO :Uninstall ) ELSE (
 IF /I "%1" EQU "-r" ( GOTO :Uninstall ) ELSE (
 IF /I "%1" EQU "/r" ( GOTO :Uninstall )))
 
-ECHO Attempting to sign WFPSampler.Exe
-%SIGNTOOL_PATH%\SignTool.exe Sign -A -V %DRIVER_BIN%
+Net Stop WFPCalloutDriver
 
-ECHO Copying WFPCalloutDriver.sys binary
+ECHO Attempting to sign %DRIVER_BIN%
+%SIGNTOOL_PATH%\SignTool.exe Sign /a /v /fd SHA256 %DRIVER_BIN%
+if %ERRORLEVEL% NEQ 0 (
+	echo Signing failed: %ERRORLEVEL%
+	EXIT /B %ERRORLEVEL%
+)
+
+ECHO Copying files
 COPY /Y %DRIVER_BIN% %WinDir%\System32\Drivers\
 COPY /Y %DRIVER_INF% %WinDir%\System32\Drivers\
 COPY /Y %DRIVER_CAT% %WinDir%\System32\Drivers\
@@ -20,6 +28,10 @@ COPY /Y %DRIVER_CERT% %WinDir%\System32\Drivers\
 
 ECHO Registering the WFPSampler Callout Driver
 RunDLL32.Exe syssetup,SetupInfObjectInstallAction DefaultInstall 132 %WinDir%\System32\Drivers\%DRIVER_INF%
+if %ERRORLEVEL% NEQ 0 (
+	echo RunDLL failed: %ERRORLEVEL%
+	EXIT /B %ERRORLEVEL%
+)
 
 ECHO Starting WFPCalloutDriver
 Net Start WFPCalloutDriver
